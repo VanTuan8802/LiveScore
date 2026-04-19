6cfad8d1ac62caa6a6fdad94ac95c463b8235c81
VanTuan8802 <vantuan8802@gmail.com>
2026-04-19 21:18:30 +0700

    init project

---
+++ LiveScore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiveScore
+//
+//  Created by VanTuan8802 on 19/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiveScore/LiveScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveScoreApp.swift
+//  LiveScore
+//
+//  Created by VanTuan8802 on 19/4/26.
+//
+
+import SwiftUI
+
+@main
+struct LiveScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
